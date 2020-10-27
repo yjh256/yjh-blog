75c1ebdf7c7d11816c86ff08bced6d959b650808
YoonJiHwan <dddalang4@naver.com>
2020-10-27 22:07:12 +0900

    10/27 업데이트(~7장)

--- src/main/java/com/yjh/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/yjh/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.yjh.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query(value = "SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/test/java/com/yjh/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/yjh/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
+
     @Test
     public void 롬복_기능_테스트() {
         //given
