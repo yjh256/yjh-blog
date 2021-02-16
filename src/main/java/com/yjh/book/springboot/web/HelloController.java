43a1499c14435bb853237936b7c30ec26e957283
YoonJiHwan <dddalang4@naver.com>
2021-02-16 16:42:48 +0900

    update(21.02.16)

--- src/main/java/com/yjh/book/springboot/web/HelloController.java
+++ src/main/java/com/yjh/book/springboot/web/HelloController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello") // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 준다.
     public String hello() {
         return "hello";
     }
--- src/test/java/com/yjh/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/yjh/book/springboot/web/HelloControllerTest.java
@@ -17,24 +17,26 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class,
+@RunWith(SpringRunner.class) // 테스트 진행 시 JUnit에 내장된 실행자 외에 다른 실행자를 실행한다.
+                             // SpringRunner.class는 스프링 부트 테스트와 JUnit 사이의 연결자 역할을 한다.
+@WebMvcTest(controllers = HelloController.class, // Web(Spring MVC)에 집중할 수 있는 스프링 테스트 annotation으로, @Controller, @ControllerAdvice 등을 사용할 수 있지만 @Service, @Component, @Repository 등은 사용할 수 없다.
         excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
         })
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired // 스프링이 관리하는 bean을 주입받는다.
+    private MockMvc mvc; // 웹 API를 테스트할 때 사용하고, 스프링 MVC 테스트의 시작점이다. 이를 통해 HTTP GET, POST 등에 대한
+                         // API 테스트를 할 수 있다.
 
     @Test
     @WithMockUser(roles = "USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청을 한다. 체이닝이 지원되어 여러 검증 기능을 이어서 선언할 수 있다.
+                .andExpect(status().isOk())  // HTTP Header의 Status를 검증한다.
+                .andExpect(content().string(hello)); // mvc.perform의 결과를 검증한다.
     }
 
     @Test
