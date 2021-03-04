f1548c8cf1c6fbb271f0f76194085ebe01319470
YoonJiHwan <dddalang4@naver.com>
2021-03-04 15:59:23 +0900

    Comments 수정 기능 추가

--- src/main/java/com/yjh/book/springboot/web/dto/comments/CommentsListResponseDto.java
+++ src/main/java/com/yjh/book/springboot/web/dto/comments/CommentsListResponseDto.java
@@ -7,11 +7,13 @@ import java.time.LocalDateTime;
 
 @Getter
 public class CommentsListResponseDto {
+    private Long id;
     private String created_by;
     private String content;
     private LocalDateTime modifiedDate;
 
     public CommentsListResponseDto(Comments entity) {
+        this.id = entity.getId();
         this.created_by = entity.getCreated_by();
         this.content = entity.getContent();
         this.modifiedDate = entity.getModifiedDate();
