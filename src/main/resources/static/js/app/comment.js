2a6bff1adc447b69df0baf6e720565b2dd72747d
YoonJiHwan <dddalang4@naver.com>
2021-03-04 17:09:30 +0900

    Comments 수정 기능 추가

--- src/main/resources/static/js/app/comment.js
+++ src/main/resources/static/js/app/comment.js
@@ -85,4 +85,11 @@ var main = {
 
 main.init();
 
-
+var only-for-author = document.querySelectorAll(".only-for-author");
+var author = document.querySelector("#author");
+for (var i = 0; i < only-for-author.length; i++) {
+    var created_by = document.querySelector(".commentsAuthor");
+    if (created_by.item(i).value == author) {
+        only-for-author.item(i).hidden = false;
+    }
+}
