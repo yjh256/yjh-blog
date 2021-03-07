e2495b525baa85731707d624c5e144f277e0b887
YoonJiHwan <dddalang4@naver.com>
2021-03-07 14:09:05 +0900

    Comments css 추가

--- src/main/java/com/yjh/book/springboot/web/dto/posts/PostsResponseDto.java
+++ src/main/java/com/yjh/book/springboot/web/dto/posts/PostsResponseDto.java
@@ -9,12 +9,12 @@ public class PostsResponseDto {
     private Long id;
     private String title;
     private String content;
-    private String classfication;
+    private String classification;
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
-        this.classfication = entity.getClassification();
+        this.classification = entity.getClassification();
     }
 }
