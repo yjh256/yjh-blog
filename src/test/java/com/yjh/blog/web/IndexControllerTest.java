4e355350ebe2e8d4ff5e47c55b2794c83126abf5
YoonJiHwan <yjh256@snu.ac.kr>
2021-08-05 16:31:38 +0900

    프론트엔드 일부 변경

--- src/test/java/com/yjh/blog/web/IndexControllerTest.java
+++ src/test/java/com/yjh/blog/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/",String.class);
 
         //then
-        assertThat(body).contains("블로그");
+        assertThat(body).contains("제목");
     }
 }
